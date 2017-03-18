b24670961104ea63812a18824c06cae3a77ff524
Levy Moreira <lmoreira@guidewire.com>
2017-03-18 18:28:41 +0000

    Translate pagination directive

--- src/main/webapp/app/components/util/jhi-item-count.directive.js
+++ src/main/webapp/app/components/util/jhi-item-count.directive.js
@@ -3,9 +3,9 @@
 
     var jhiItemCount = {
         template: '<div class="info">' +
-                    'Showing {{(($ctrl.page - 1) * $ctrl.itemsPerPage) == 0 ? 1 : (($ctrl.page - 1) * $ctrl.itemsPerPage + 1)}} - ' +
+                    '<span data-translate="global.pagination.showing"></span> {{(($ctrl.page - 1) * $ctrl.itemsPerPage) == 0 ? 1 : (($ctrl.page - 1) * $ctrl.itemsPerPage + 1)}} - ' +
                     '{{($ctrl.page * $ctrl.itemsPerPage) < $ctrl.queryCount ? ($ctrl.page * $ctrl.itemsPerPage) : $ctrl.queryCount}} ' +
-                    'of {{$ctrl.queryCount}} items.' +
+                    '<span data-translate="global.pagination.of"></span> {{$ctrl.queryCount}} <span data-translate="global.pagination.items"></span>.' +
                 '</div>',
         bindings: {
             page: '<',
