af8a62b3a5f7e5d246165094d6e5efbd83f8cf34
Levy Moreira <lmoreira@guidewire.com>
2017-03-18 17:31:20 +0000

    Removed wrong file Update logo Remove hipster image Update app title Add initial version of transaction page

--- src/main/webapp/app/entities/transaction/transaction.controller.js
+++ src/main/webapp/app/entities/transaction/transaction.controller.js
@@ -11,15 +11,44 @@
 
         var vm = this;
 
+        //Filters
+        vm.year = new Date().getFullYear();
+        vm.month = (new Date().getMonth()+1).toString();
+
+        vm.years = [];
+        var i;
+        for(i= 1899; i<=vm.year+100; i++)
+            vm.years.push(i);
+
+
         vm.transactions = [];
+        vm.clear = clear;
+        vm.search = search;
+        vm.loadAll = loadAll;
 
         loadAll();
 
         function loadAll() {
             Transaction.query(function(result) {
                 vm.transactions = result;
-                vm.searchQuery = null;
             });
         }
-    }
+
+        function search() {
+            console.log(vm.description);
+            console.log(vm.account);
+            debugger;
+            /*if (!vm.searchQuery) {
+                return vm.loadAll();
+            }
+            Transaction.query({query: vm.searchQuery}, function(result) {
+                vm.transactions = result;
+                vm.currentSearch = vm.searchQuery;
+            });*/
+        }
+
+        function clear() {
+            vm.searchQuery = null;
+            loadAll();
+        }    }
 })();
